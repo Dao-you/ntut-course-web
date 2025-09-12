5c05aa0e2f4f2574010b7d6209c7efd97b6241a3
Dao-you <67760701+Dao-you@users.noreply.github.com>
2025-09-13 05:51:18 +0800

    refactor: expose api helper via CommonJS (#10)

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-export default function createApi(apiBase) {
+function createApi(apiBase) {
   const apiUrl = (path) => {
     const base = apiBase.endsWith('/') ? apiBase.slice(0, -1) : apiBase
     const normalizedPath = path.startsWith('/') ? path : `/${path}`
@@ -6,3 +6,6 @@ export default function createApi(apiBase) {
   }
   return { apiBase, apiUrl }
 }
+
+module.exports = createApi
+module.exports.default = createApi
